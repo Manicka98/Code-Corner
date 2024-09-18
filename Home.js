7fcde72db4bf738aa02e271516692bcb2c8554cc
Manicka98 <manickaraj1097@gmail.com>
2024-09-18 21:26:02 +0530

    version 0.2

--- Home.js
+++ Home.js
@@ -1,6 +1,9 @@
 const mbtn = document.getElementById("list_btn");
 const nlist = document.getElementById("nav_list");
 mbtn.addEventListener("click",()=>{
+    console.log(mbtn)
     
 nlist.classList.toggle("show_menu")
 })
+
+
